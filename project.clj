326984ff7b6985f2432b4d33ae7b614eae6f8ace
Алексей <a@guava.by>
2017-01-03 06:39:37 +0300

    add images serving

--- project.clj
+++ project.clj
@@ -1,6 +1,14 @@
-(defproject clojure-images-server "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject clojure-images-server "0.1.0"
+  :description "Server for react-images-uploader"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [ring/ring-core "1.5.0"]
+                 [ring "1.5.0"]
+                 [compojure "1.5.1"]
+                 [levand/immuconf "0.1.0"]]
+  :plugins [[lein-ring "0.10.0"]]
+  :ring {:handler clojure-images-server.core/app
+         :auto-reload? true
+         :auto-refresh? true
+         :reload-paths ["src/clojure_images_server"]})
--- src/clojure_images_server/core.clj
+++ src/clojure_images_server/core.clj
@@ -1,6 +1,23 @@
-(ns clojure-images-server.core)
+(ns clojure-images-server.core
+  (:require [compojure.core :refer :all]
+            [compojure.route :as route]
+            [immuconf.config])
+  (:use [ring.util.response]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def cfg (immuconf.config/load "config.edn"))
+
+(def my-routes
+ (immuconf.config/get cfg :routes))
+
+(defroutes serve-routes
+  (apply routes
+    (map
+     #(GET
+       (str (:servepath %) "/:file") [file]
+         (resource-response file {:root (:fileserve %)}))
+     my-routes)))
+
+(defroutes app-routes
+  (route/not-found (resource-response "404.html" {:root "static"})))
+
+(def app (routes serve-routes app-routes))
