87dd9c2422c89fcc5a7f64d774a8910a598002f6
Алексей <a@guava.by>
2017-01-07 05:28:46 +0300

    fix race condition

--- src/clojure_images_server/save.clj
+++ src/clojure_images_server/save.clj
@@ -2,24 +2,21 @@
   (:require [ring.middleware.multipart-params :as mp])
   (:use [ring.util.json-response]))
 
-(def route-settings)
-
 (defn- get-files [req]
   (get (:params (mp/multipart-params-request req)) "imageFiles"))
 
 (defn- save-file [file]
   (:filename file))
 
-(defn- save-files [files]
-  (if (map? files)
-      (list (save-file files))
-      (if (:multiple route-settings)
-        (map save-file files)
-        (list (save-file (get files 0))))))
+(defn- save-files [rs files]
+  (do (println rs)
+    (if (map? files)
+        (list (save-file files))
+        (if (:multiple rs)
+          (map save-file files)
+          (list (save-file (get files 0)))))))
 
 (defn handler [req rs]
-  (do
-    (def route-settings rs)
-    (-> (get-files req)
-        (save-files)
-        (json-response))))
+  (-> (get-files req)
+      ((partial save-files rs))
+      (json-response)))
